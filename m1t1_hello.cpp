bbbadeec442d3326a3c32bcd7de107cebc4bf503
Aly3673 <younga3537@student.faytechcc.edu>
2025-08-20 15:37:22 +0000

    Finished first program. Outputs "Hello World" and "My foo is C++". Program runs successfully.

---
+++ m1t1_hello.cpp
@@ -0,0 +1,17 @@
+/*
+CSC 134
+M1T1 - Hello World
+A Young
+8/20/25
+First program.
+*/
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; // "end-elle"
+    cout << "My foo is C++" << endl; // Added in my foo
+    return 0;
+}
+
