4ec12cbc16cc67752f42b4fbe7d330fda2ca2f78
Aly3673 <younga3537@student.faytechcc.edu>
2025-08-25 15:04:19 +0000

    Program successfully runs code and outputs "Hello, user!"

---
+++ m1t1.cpp
@@ -0,0 +1,22 @@
+/*
+This is a comment in C++.
+Alyssa Y., 8/25/25
+ 
+m1t1 - hello, user.
+INPUT: Run the program.
+PROCESS: None yet.
+OUTPUT: Return 0 (no error)
+SPECIAL FIX: Greet the user.
+*/
+
+// Magic words to start.
+#include <iostream>
+using namespace std;
+
+int main() {
+    // SFX: Greet user
+    cout << "Hello, user!" <<endl;
+
+    // Output
+    return 0; // No errors
+}
