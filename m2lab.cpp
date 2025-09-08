2be45215f77985dd6c71b43b3fa10ab7f64cec5a
Aly3673 <younga3537@student.faytechcc.edu>
2025-09-08 14:36:20 +0000

    Started on the crate program. Put in variables and commented structure.

---
+++ m2lab.cpp
@@ -0,0 +1,33 @@
+/*
+CSC 134
+M2LAB
+A. Young
+9/8/25
+Crate charging program.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Set up the variables
+    double  length, width, height; // Size of crate
+    double cost_to_make, charge_to_customer;
+    double profit;
+
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    // Get the crate info
+
+
+
+    // Calculate the cost and prices
+
+
+
+    // Display the results
+
+
+
+    return 0;
+}
