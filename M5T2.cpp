914910c2779d1216f96f28c1140e02435e3b0369
Aly3673 <younga3537@student.faytechcc.edu>
2025-11-12 15:57:38 +0000

    Finished doing part two out of three of M5T2. Started a bit on part three.

--- M5T2.cpp
+++ M5T2.cpp
@@ -8,16 +8,30 @@ Alyssa Y.
 #include <iostream>
 using namespace std;
 
+// Given 2 numbers, print them out nicely on a line.
+void printResult(int number, int result) {
+    cout << number << " squared = " << result << endl;
+}
+
+// Given a number, return the square of it.
+int square(int number) {
+    int result;
+    
+    return result;
+
+}
+
 int main() {
     // Count some numbers
     // And find a result for each
-    int count = 0;
+    int count = 1;
     int result;
+    
+    // This version will be squares.
 
-    while (count < 12) {
-        // What is 100 times the count?
-        result = 10 * count;
-        cout << count << " " << result << endl;
+    while (count <= 10) {
+        result = square(count);
+        printResult(count, result); // Gets calculations from above in void.
         count++;
     }
 }
