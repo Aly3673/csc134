eae73d2b117f7221ff3be6e5009cb6c26a452532
Aly3673 <younga3537@student.faytechcc.edu>
2025-12-02 02:12:02 +0000

    Started on M2LAB2.

---
+++ M2LAB2.py
@@ -0,0 +1,15 @@
+"""
+CSC 134, M6LAB1
+Alyssa Y.
+12/1/25
+Buckshot Roulette.
+"""
+
+import turtle
+win = turtle.Screen()
+t = turtle.Turtle()
+
+# Customizing 
+t.pensize(4)
+t.pencolor("pink")
+t.shape("turtle")
