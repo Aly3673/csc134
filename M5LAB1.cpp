754c9a2a96eb4c5e98e0f020f87ac05505e3d0ed
Aly3673 <younga3537@student.faytechcc.edu>
2025-12-08 00:39:49 +0000

    Did some work on M5LAB1.

--- M5LAB1.cpp
+++ M5LAB1.cpp
@@ -9,19 +9,46 @@ Choose Your Own Adventure.
 using namespace std;
 
 // Variables -- Function Choices.
+void game_Start();
 void choice_House();
 void choice_Tombstone();
 void choice_Shed();
 
 int main() {
-    cout << R"(You've been running through the woods for hours, trying to escape the a serial killer that is detemined to get you.
-You're exhausted and take a moment to catch your breath. Luckily for you, you came across three different structures to hide in.
-You come across: )" << endl; // R for raw print; Prints every line as is until it ends.
+    cout << "==========================" << endl;
+    cout << "       Killer Chase       " << endl;
+    cout << "==========================" << endl;
     cout << endl;
-    cout << "1. A House" << endl;
+    cout << "Options:\n1. Start Game\n2. Quit Program" << endl;
+    cout << "Please choose an option: ";
+    int gameMenuInput;
+
+    while (gameMenuInput != 1 || gameMenuInput != 2) {
+        cin >> gameMenuInput;
+
+        if (gameMenuInput == 1) {
+            game_Start();
+            break;
+        }
+        else if (gameMenuInput == 2) {
+            cout << endl;
+            cout << "Goodbye!" << endl;
+            return 0;
+        }
+        else {
+            cout << "Invalid option. Please select a valid option: ";
+        }
+    }
+}
+
+void game_Start() {
+    cout << endl;
+    cout << "You've been running through the woods for hours, trying to escape the a serial killer that is detemined to get you.\nYou're exhausted and take a moment to catch your breath. Luckily for you, you came across three different structures to hide in.\nYou come across:" << endl;
+    cout << endl;
+    cout << "1. A Small House" << endl;
     cout << "2. A Tombstone" << endl;
     cout << "3. A Shed" << endl;
-    cout << "Extra: 4. Quit" << endl;
+    cout << "(4. Quit Program)" << endl;
     cout << endl;
 
     cout << "What do you choose to do?" << endl;
@@ -49,7 +76,7 @@ You come across: )" << endl; // R for raw print; Prints every line as is until i
         else if (menu_playerChoice == 4) {
             cout << endl;
             cout << "Goodbye!" << endl;
-            return 0;
+            exit(0); // Learned this neat feature today.
         }
         else {
             cin.clear();
@@ -59,11 +86,14 @@ You come across: )" << endl; // R for raw print; Prints every line as is until i
 }
 
 void choice_House() {
-    cout << "Going into the house, you are greeted by three rooms. Which room do you hide in? " << endl;
+    cout << "Going into the house, you are greeted by three rooms.\n1. Pantry\n2. Bathroom\n3. Bedroom" << endl; 
+    cout << "Which room do you hide in? ";
     int house_playerChoice;
-    cin >> house_playerChoice;
 
-    
+    while (house_playerChoice != 1, house_playerChoice != 2, house_playerChoice != 3) {
+        cin >> house_playerChoice;
+        
+    }
 }
 
 void choice_Tombstone() {
