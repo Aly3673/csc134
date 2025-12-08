72286cca42f61c2d5fc3ae2ba7ac0e1db64dbd6e
Aly3673 <younga3537@student.faytechcc.edu>
2025-12-08 17:52:32 +0000

    Updated M2LAB2, used Python Sandbox.

--- M2LAB2.py
+++ M2LAB2.py
@@ -13,3 +13,73 @@ t = turtle.Turtle()
 t.pensize(4)
 t.pencolor("pink")
 t.shape("turtle")
+
+# First - Flower
+t.penup()
+t.goto(-150, -150)
+t.pendown()
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+t.setheading(55)
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+t.setheading(110)
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+t.setheading(165)
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+t.setheading(220)
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+t.setheading(275)
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+t.setheading(315)
+for petal in range(1):
+  t.circle(50, 60)
+  t.left(120)
+  t.circle(50,60)
+  t.left(180 - 60)
+
+# Second - Star
+t.penup()
+t.goto(150, 150)
+t.pencolor("yellow")
+t.pendown()
+for star in range(5):
+  t.forward(100)
+  t.right(144)
+  
+# Third - Spiral
+t.penup()
+t.goto(-150, 150)
+t.pencolor("orange")
+t.pendown()
+for spiral in range(50):
+  t.forward(spiral)
+  t.circle(5,50)
+
+# Fourth - Polygon
+t.penup()
+t.goto(150, -150)
+t.pencolor("blue")
+t.pendown()
